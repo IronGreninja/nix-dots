c9a45a559e8f0299426ffc2ea1c880281683b141
IronGreninja <92992745+IronGreninja@users.noreply.github.com>
2026-05-25 20:00:30 +0530

    misc: comment new info

--- config/apps/wezterm/default.nix
+++ config/apps/wezterm/default.nix
@@ -5,6 +5,8 @@
         enable = true;
         extraConfig = builtins.readFile ./wezterm.lua;
       };
+      # https://docs.vicinae.com/faq#how-to-set-which-terminal-to-use-to-launch-terminal-apps
+      xdg.configFile."xdg-terminals.list".text = "org.wezfurlong.wezterm.desktop";
     };
   };
 }
