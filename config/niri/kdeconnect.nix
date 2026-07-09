f46d2a450c81bb8224dca3ad8bda8075aba1eb7b
IronGreninja <92992745+IronGreninja@users.noreply.github.com>
2026-07-09 18:38:18 +0530

    fix: noctalia v5: updated keybinds; kdeconnect service

--- config/niri/kdeconnect.nix
+++ config/niri/kdeconnect.nix
@@ -2,13 +2,13 @@
   ig.wm._.niri = {
     nixos.programs.kdeconnect.enable = true;
     homeManager = {pkgs, ...}: {
-      systemd.user.services.niri-wants-kdeconnectd = {
+      systemd.user.services.niri-wants-kdeconnect = {
         Unit = {
           Description = "kdeconnect for niri";
           After = "niri.service";
         };
         Service = {
-          ExecStart = "${pkgs.kdePackages.kdeconnect-kde}/bin/kdeconnectd";
+          ExecStart = "${pkgs.kdePackages.kdeconnect-kde}/bin/kdeconnect-indicator"; # starts kdeconnectd + tray icon
           Restart = "on-failure";
         };
         Install.WantedBy = ["niri.service"];
